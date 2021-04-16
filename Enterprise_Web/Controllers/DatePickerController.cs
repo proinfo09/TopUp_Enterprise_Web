de594eb666e1ce84f76af61bbded641b9c5ffdd9
phuc khang <khangnpgcs18036@fpt.edu.vn>
2021-04-16 21:05:42 +0700

    Upload datepicker and chart report

---
+++ Enterprise_Web/Controllers/DatePickerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Enterprise_Web.Controllers
+{
+    public class DatePickerController : Controller
+    {
+        // GET: DatePicker
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
