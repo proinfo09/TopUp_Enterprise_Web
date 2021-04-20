b5fe125a38e4393c3bae34ba7fe797b71cd5701c
proinfo09 <63229875+proinfo09@users.noreply.github.com>
2021-04-20 14:44:14 +0700

    Update

--- Enterprise_Web/Controllers/HomeController.cs
+++ Enterprise_Web/Controllers/HomeController.cs
@@ -17,32 +17,6 @@ namespace Enterprise_Web.Controllers
     public class HomeController : Controller
     {
         private WebEnterpriseEntities db = new WebEnterpriseEntities();
-        public ActionResult Dashboard()
-        {
-            AspNetUser user = db.AspNetUsers.Find(User.Identity.GetUserId());
-            var role = user.AspNetRoles.FirstOrDefault();
-            switch (role.Id)
-            {
-                case "1": //if role is admin 
-                    return RedirectToRoute("User_Admin_Detail/AdminDashboard");
-                    break;
-                case "2": //if role is admin 
-                    return Redirect("User_Marketing_Manager_Detail/MmDashboard");
-                    break;
-                case "3": //if role is admin 
-                    return RedirectToRoute("User_Marketing_Coordinator_Detail/McDashboard");
-                    break;
-                case "4": //if role is admin 
-                    return RedirectToAction("StudentDashboard");
-                    break;
-                case "5": //if role is admin 
-                    return Redirect("User_Guest_Detail/GuestDashboard");
-                    break;
-                default:
-                    break;
-            }
-            return View();
-        }
         public ActionResult Index()
         {
             AspNetUser user = db.AspNetUsers.Find(User.Identity.GetUserId());
